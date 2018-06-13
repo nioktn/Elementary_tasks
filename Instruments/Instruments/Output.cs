1e488d7ed193b5a9ea8c772516c145416a8bc616
n1kitan <nioktn@gmail.com>
2018-06-13 13:32:41 +0300

    Added Instruments library

---
+++ Instruments/Instruments/Output.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Instruments
+{
+    public static class Output
+    {
+        public static void Message(String msg, ConsoleColor clr)
+        {
+            Console.ForegroundColor = clr;
+            Console.WriteLine(msg + "\n");
+            Console.ResetColor();
+        }
+
+        public static void WriteFraction(double num)
+        {
+            
+            String numStr = num.ToString();
+            String[] numArray = numStr.Split('.');
+            int length = numArray.Length;
+            num = num * Math.Pow(10, length);
+
+        }
+    }
+}
---
+++ Instruments/Instruments/Validator.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Instruments
+{
+    public static class Validator
+    {
+        public static string ReadString()
+        {
+            return Console.ReadLine();
+        }
+        public static string ClearString(String str)
+        {
+            return str.Replace(" ", String.Empty).Replace("\t", String.Empty);
+        }
+        public static int ReadInt()
+        {
+            String temp = Console.ReadLine();
+            temp = temp.Trim().Replace(" ", String.Empty).Replace("\t", String.Empty);
+            int result;
+            try
+            {
+                result = Int32.Parse(temp);
+            }
+            catch
+            {
+                throw;
+            }
+            return result;
+        }
+        public static long ReadLong()
+        {
+            String temp = Console.ReadLine();
+            temp = temp.Trim().Replace(" ", String.Empty).Replace("\t", String.Empty);
+            long result;
+            try
+            {
+                result = long.Parse(temp);
+            }
+            catch
+            {
+                throw;
+            }
+            return result;
+        }
+        public static double ReadDouble()
+        {
+            String temp = Console.ReadLine();
+            temp = temp.Trim().Replace(" ", String.Empty).Replace("\t", String.Empty);
+            double result;
+            try
+            {
+                result = Double.Parse(temp);
+            }
+            catch
+            {
+                throw;
+            }
+            return result;
+        }
+        public static char ReadChar()
+        {
+            String temp = Console.ReadLine();
+            char result;
+            try
+            {
+                result = Char.Parse(temp.Replace(" ", String.Empty).Replace("\t", String.Empty).Substring(0, 1));
+            }
+            catch
+            {
+                throw;
+            }
+            return result;
+        }
+        public static bool IsPositive(int value)
+        {
+            bool result = value > 0 ? true : false;
+            if (!result)
+            {
+                throw new Exception("Value can`t be negative");
+            }
+            return result;
+        }
+        public static bool IsPositive(double value)
+        {
+            bool result = value > 0 ? true : false;
+            if (!result)
+            {
+                throw new Exception("Value can`t be negative");
+            }
+            return result;
+        }
+
+        public static double ReadFraction()
+        {
+            String fractionStr = Validator.ReadString();
+            String[] splitArray = fractionStr.Split('/');
+            double numerator = double.Parse(splitArray[0]);
+            double nominator = double.Parse(splitArray[1]);
+            return numerator / nominator;
+        }
+    }
+}
