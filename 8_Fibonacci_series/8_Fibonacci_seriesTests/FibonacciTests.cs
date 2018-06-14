34a5d992d11ca894a9e57a7c656fa651121dbafa
n1kitan <nioktn@gmail.com>
2018-06-14 22:16:18 +0300

    Added unit tests to 8_Fibonacci_series project
    Added unit tests on CalcFbnc method to 8_Fibonacci_series project.

---
+++ 8_Fibonacci_series/8_Fibonacci_seriesTests/FibonacciTests.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using _8_Fibonacci_series;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _8_Fibonacci_seriesTests
+{
+    [TestClass()]
+    public class FibonacciTests
+    {
+        [TestMethod()]
+        public void CalcFbncTest_15_1500_list()
+        {
+            long min = 15;
+            long max = 1500;
+            List<long> expectedResult = new List<long> { 21, 34, 55, 89, 144, 233, 377, 610, 987 };
+            List<long> actualResult = Fibonacci.CalcFbnc(min, max);
+            CollectionAssert.AreEqual(expectedResult, actualResult);
+        }
+
+        [TestMethod()]
+        public void CalcFbncTest_10()
+        {
+            long numLength = 10;
+            List<long> expectedResult = new List<long> { 1134903170, 1836311903, 2971215073, 4807526976, 7778742049 };
+            List<long> actualResult = Fibonacci.CalcFbnc(numLength);
+            CollectionAssert.AreEqual(expectedResult, actualResult);
+        }
+    }
+}
