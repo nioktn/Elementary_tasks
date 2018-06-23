9aff852ccfb0ea5abd8fb5d0e46787117bfbee08
Nikita Nikitin <nioktn@gmail.com>
2018-06-23 22:48:44 +0300

    Some fixes
    Some fixes.

--- 2_conv_analyze/2_conv_analyze/Program.cs
+++ 2_conv_analyze/2_conv_analyze/Program.cs
@@ -8,46 +8,33 @@ namespace _2_conv_analyze
         static void Main(string[] args)
         {
             string answer;
-            Envelope[] envs = new Envelope[1];
             for (; ; )
             {
-                try
-                {
-                    Console.Write(" First: ");
-                    Envelope first = CreateEnvelope();
-                    Console.Write("\n Second: ");
-                    Envelope second = CreateEnvelope();
-                    if (first > second || first < second || Envelope.DiagCmp(first, second))
-                        Output.Message("Can be enclosed", ConsoleColor.Yellow);
-                    else
-                        Output.Message("Can`t be enclosed", ConsoleColor.Yellow);
+                Console.Write(" First: ");
+                Envelope first = CreateEnvelope();
+                Console.Write("\n Second: ");
+                Envelope second = CreateEnvelope();
+                if (first > second || first < second || Envelope.DiagCmp(first, second))
+                    Output.Message("Can be enclosed", ConsoleColor.Yellow);
+                else
+                    Output.Message("Can`t be enclosed", ConsoleColor.Yellow);
 
-                    Console.Write("Do you wanna continue ? \n >> ");
-                    answer = Validator.ReadString().ToLower().Trim();
-                    if (answer.Equals("y") || answer.Equals("yes"))
-                    {
-                        Console.WriteLine("\n");
-                        continue;
-                    }
-                    else
-                        Environment.Exit(0);
-                }
-                catch (Exception ex)
+                Console.Write("Do you wanna continue ? \n >> ");
+                answer = Validator.ReadString().ToLower().Trim();
+                if (answer.Equals("y") || answer.Equals("yes"))
                 {
-                    Output.Message(ex.Message, ConsoleColor.Red);
+                    Console.WriteLine("\n");
                     continue;
                 }
+                else Environment.Exit(0);
             }
         }
         public static Envelope CreateEnvelope()
         {
-            double side1 = 0, side2 = 0, value;
-            Console.Write("\nEnter first side: \n >> ");
-            value = Validator.ReadDouble();
-            if (Validator.IsPositive(value)) side1 = value;
-            Console.Write("\nEnter second side: \n >> ");
-            value = Validator.ReadDouble();
-            if (Validator.IsPositive(value)) side2 = value;
+            Console.Write("\nEnter first side: \n");
+            double side1 = Validator.ReadDouble2(true);
+            Console.Write("\nEnter second side: \n");
+            double side2 = Validator.ReadDouble2(true);
             return new Envelope(side1, side2);
         }
     }
--- 4_file_parser/4_file_parser/Program.cs
+++ 4_file_parser/4_file_parser/Program.cs
@@ -15,38 +15,30 @@ namespace _4_file_parser
             {
                 for (; ; )
                 {
-                    int choise;
-                    Console.Write("Choose programm mode:\n 1. Entries search\n 2. Replace string\n 0. Exit\n >> ");
-                    try
+                    int option;
+                    Console.Write("Choose programm mode:\n 1. Entries search\n 2. Replace string\n 0. Exit\n");
+                    option = Validator.ReadInt2();
+                    switch (option)
                     {
-                        choise = Validator.ReadInt();
-                        switch (choise)
-                        {
-                            case (1):
-                                String search;
-                                Console.Write(" \nEntries search:\nEnter your string for search:\n >> ");
-                                search = Validator.ReadString();
-                                Output.Message("There are " + FileParser.EntriesQty(filePath, search) + " entries of your string\n", ConsoleColor.Yellow);
-                                break;
-                            case (2):
-                                Console.Write("Enter pattern string: \n >> ");
-                                string pattern = Validator.ReadString();
-                                Console.Write("Enter replace string: \n >> ");
-                                string replace = Validator.ReadString();
-                                FileParser.ReplaceString(filePath, pattern, replace);
-                                break;
-                            case (0):
-                                Environment.Exit(0);
-                                break;
-                            default:
-                                Output.Message("There is no mode like this.\n", ConsoleColor.Red);
-                                continue;
-                        }
-                    }
-                    catch (Exception ex)
-                    {
-                        Output.Message(ex.Message, ConsoleColor.Red);
-                        continue;
+                        case (1):
+                            String search;
+                            Console.Write(" \nEntries search:\nEnter your string for search:\n >> ");
+                            search = Validator.ReadString();
+                            Output.Message("There are " + FileParser.EntriesQty(filePath, search) + " entries of your string\n", ConsoleColor.Yellow);
+                            break;
+                        case (2):
+                            Console.Write("Enter pattern string: \n >> ");
+                            string pattern = Validator.ReadString();
+                            Console.Write("Enter replace string: \n >> ");
+                            string replace = Validator.ReadString();
+                            FileParser.ReplaceString(filePath, pattern, replace);
+                            break;
+                        case (0):
+                            Environment.Exit(0);
+                            break;
+                        default:
+                            Output.Message("There is no mode like this.\n", ConsoleColor.Red);
+                            continue;
                     }
                 }
             }
